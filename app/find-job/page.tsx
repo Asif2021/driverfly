322584e9cac4d634869b2324ba12996052962e07
asif <asiframzan2021@gmail.com>
2024-07-01 13:31:45 +0500

    find-job page added

---
+++ app/find-job/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const findJob = () => {
+  return (
+    <div>findJob</div>
+  )
+}
+
+export default findJob
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+"use client"
 import styles from "./page.module.css";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
   return (
     <main className={styles.main}>
-      <Button variant="success">Find Job</Button>
+      <Button variant="success" onClick={()=>router.push("/find-job")}>Find Job</Button>
     </main>
   );
 }
